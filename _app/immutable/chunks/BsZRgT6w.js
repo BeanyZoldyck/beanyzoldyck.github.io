eb2d10fcbb7b9b7ed203752bed522520d4de627c
BeanyZoldyck <beanyzoldyck@gmail.com>
2026-09-14 16:47:17 -0400

    Updates

--- _app/immutable/chunks/BsZRgT6w.js
+++
@@ -1,1 +0,0 @@
-import{ad as a}from"./C4Dvzcg6.js";a();
---
+++ _app/immutable/nodes/6.fmlFu52j.js
@@ -0,0 +1,45 @@
+import{b as w,a as z,f as A}from"../chunks/UoIFF554.js";import"../chunks/DibLlfBe.js";import{F as H,h as g,e as x,a4 as m,a as b,o as T,a5 as R,j as M,a2 as S,a3 as N,a6 as P,a7 as V,a8 as j,a9 as G,aa as O,q as L,z as B,B as h,y as f,C as c,g as y,Y as q}from"../chunks/CUKMfxNv.js";import{e as D,i as Y}from"../chunks/BEdAIbC5.js";import{i as Z}from"../chunks/tFFtzN7e.js";import{s as F}from"../chunks/D5ktsEFD.js";import{s as k}from"../chunks/VCP1rMIm.js";import{r as I}from"../chunks/D9oEtbJD.js";function U(d,r,s=!1,e=!1,a=!1,v=!1){var n=d,o="";if(s){var l=d;g&&(n=x(m(l)))}H(()=>{var i=T;if(o===(o=r()??"")){g&&b();return}if(s&&!g){i.nodes=null,l.innerHTML=o,o!==""&&w(m(l),l.lastChild);return}if(i.nodes!==null&&(R(i.nodes.start,i.nodes.end),i.nodes=null),o!==""){if(g){M.data;for(var t=b(),_=t;t!==null&&(t.nodeType!==S||t.data!=="");)_=t,t=N(t);if(t===null)throw P(),V;w(M,_),n=x(t);return}var E=e?G:a?O:void 0,u=j(e?"svg":a?"math":"template",E);u.innerHTML=o;var p=e||a?u:u.content;if(w(m(p),p.lastChild),e||a)for(;m(p);)n.before(m(p));else n.before(p)}})}var J=A('<div class="group relative mr-0"><a class=" text-purple-300 hover:text-purple-200"> <!></a></div>');function C(d,r){L(r,!0);var s=J(),e=h(s),a=h(e),v=f(a);U(v,()=>r.contact.svg),c(e),c(s),H(n=>{k(e,"href",r.contact.link),k(e,"target",n),k(e,"title",r.contact.name),F(a,`${r.contact.name??""} `)},[()=>r.contact.link.includes("email")?"":"_blank"]),z(d,s),B()}var K=A('<section class="py-20"><h1 class="mb-8 text-4xl font-bold text-purple-300">contact</h1> <div class="space-y-8"><div class="border-l-2 border-purple-700 pl-6"><p class="mb-2 text-lg text-purple-400">feel free to reach out:</p> <!></div> <div class="border-l-2 border-purple-700 pl-6"><p class="mb-4 text-lg text-purple-400">find me online:</p> <div class="grid w-100 grid-cols-2 space-y-4"></div></div></div></section>');function oe(d,r){L(r,!1);const s=[{name:"github",link:"https://github.com/BeanyZoldyck",svg:`<svg
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="1.5"
+							shape-rendering="geometricPrecision"
+							viewBox="0 0 24 24"
+							height="60"
+							width="60"
+							style="color: currentcolor;"
+							><path
+								d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"
+							></path></svg
+						>`},{name:"linkedin",link:"https://linkedin.com/in/chukaez",svg:`<svg
+						fill="none"
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="1.5"
+						shape-rendering="geometricPrecision"
+						viewBox="0 0 24 24"
+						height="60"
+						width="60"
+						style="color: currentcolor;"
+						><path
+							d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+						></path><circle cx="4" cy="4" r="2"></circle></svg
+					>`},{name:"twitter",link:"https://x.com/chuka_ze",svg:`<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+</svg>`},{name:"devpost",link:"https://devpost.com/cezeoke",svg:`<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg stroke="currentColor" fill="#000000" width="70px" height="70px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><path d="M6.002 1.61 0 12.004 6.002 22.39h11.996L24 12.004 17.998 1.61zm1.593 4.084h3.947c3.605 0 6.276 1.695 6.276 6.31 0 4.436-3.21 6.302-6.456 6.302H7.595zm2.517 2.449v7.714h1.241c2.646 0 3.862-1.55 3.862-3.861.009-2.569-1.096-3.853-3.767-3.853z"/></svg>`}];Z();var e=K(),a=f(h(e),2),v=h(a),n=f(h(v),2);{let i=q(()=>({name:"email",link:I("/email"),svg:`<svg
+					fill="none"
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1.5"
+					shape-rendering="geometricPrecision"
+					viewBox="0 0 24 24"
+					height="40"
+					width="40"
+					style="color: currentcolor;"
+					><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+					></path><path d="M22 6l-10 7L2 6"></path></svg
+				>`}));C(n,{get contact(){return y(i)}})}c(v);var o=f(v,2),l=f(h(o),2);D(l,5,()=>s.sort(()=>Math.random()-.5),Y,(i,t)=>{C(i,{get contact(){return y(t)}})}),c(l),c(o),c(a),c(e),z(d,e),B()}export{oe as component};
